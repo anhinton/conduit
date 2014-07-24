de02a1101c0140527d9021ab10a19a12b665696b
Ashley Noel Hinton <ashley@canadia.co.nz>
2014-07-25 09:52:51 +1200

    added amendSeatchPaths()

--- R/search.R
+++ R/search.R
@@ -14,6 +14,21 @@ splitPaths <- function(x) {
     strsplit(x, "|", fixed=TRUE)[[1]]
 }
 
+## amend paths to be searched
+amendSearchPaths <- function(searchPaths, newPaths) {
+    ## if newPaths starts with pathSep, append to searchPaths
+    if (grepl(paste0("^[", pathSep, "]"), newPaths)) {
+        paste0(searchPaths, newPaths)
+    ## else if newPaths ends with pathSep, prepend to searchPaths
+    } else if (grepl(paste0("[", pathSep, "]$"), newPaths)) {
+        paste0(newPaths, searchPaths)
+    ## else return only newPaths
+    } else {
+        newPaths
+    }
+}
 
-
-splitPaths(defaultSearchPaths)
+searchPaths <- defaultSearchPaths
+newPaths <- "|~/Desktop|~/Pictures"
+searchPaths <- amendSearchPaths(searchPaths, newPaths)
+splitPaths(searchPaths)
