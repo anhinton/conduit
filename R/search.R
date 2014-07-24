de71a783f2325e98b6751e06937f7564c85a8bf6
Ashley Noel Hinton <ashley@canadia.co.nz>
2014-07-25 10:50:13 +1200

    added findFile()

--- R/search.R
+++ R/search.R
@@ -11,24 +11,50 @@ defaultSearchPaths <- paste(".//", "${ROOT}", sep=pathSep)
 
 ## split search paths string
 splitPaths <- function(x) {
-    strsplit(x, "|", fixed=TRUE)[[1]]
+    strsplit(x, pathSep, fixed=TRUE)[[1]]
 }
 
 ## amend paths to be searched
 amendSearchPaths <- function(searchPaths, newPaths) {
-    ## if newPaths starts with pathSep, append to searchPaths
-    if (grepl(paste0("^[", pathSep, "]"), newPaths)) {
-        paste0(searchPaths, newPaths)
-    ## else if newPaths ends with pathSep, prepend to searchPaths
-    } else if (grepl(paste0("[", pathSep, "]$"), newPaths)) {
-        paste0(newPaths, searchPaths)
-    ## else return only newPaths
-    } else {
-        newPaths
-    }
+    searchPaths <- 
+        ## if newPaths starts with pathSep, append to searchPaths
+        if (grepl(paste0("^[", pathSep, "]"), newPaths)) {
+            paste0(searchPaths, newPaths)
+            ## else if newPaths ends with pathSep, prepend to searchPaths
+        } else if (grepl(paste0("[", pathSep, "]$"), newPaths)) {
+            paste0(newPaths, searchPaths)
+            ## else return only newPaths
+        } else {
+            newPaths
+        }
 }
 
-searchPaths <- defaultSearchPaths
-newPaths <- "|~/Desktop|~/Pictures"
-searchPaths <- amendSearchPaths(searchPaths, newPaths)
-splitPaths(searchPaths)
+## expandSearchPath to absolute file path
+expandSearchPaths <- function(s) {
+    s <- gsub("${ROOT}", getwd(), s, fixed=TRUE)
+    normalizePath(s)
+}
+
+findFile <- function (x, searchPaths) {
+    searchPaths <- amendSearchPaths(defaultSearchPaths, searchPaths)
+    searchPaths <- splitPaths(searchPaths)
+    searchPaths <- unique(expandSearchPaths(searchPaths))
+    result <- NULL
+    count <- 1
+    while (is.null(result) && count <= length(searchPaths)) {
+        files <- list.files(path=searchPaths[count], pattern=paste0(x,"$"),
+                            full.names=TRUE, recursive=TRUE)
+        result <-
+            if (length(files) > 0) {
+                if (length(files) > 1) {
+                    warning(paste(paste("Found more than one match for",
+                                        x), files, collapse="\n"))
+                }
+                files[1]
+            } else {
+                NULL
+            }
+        count <- count + 1;
+    }
+    result
+}
