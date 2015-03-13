9a338a13c8925d01deb3f136301e36aea624cd74
Ashley Noel Hinton <ahin017@aucklanduni.ac.nz>
2015-03-13 17:11:12 +1300

    added fileIO()

--- R/inputOutput.R
+++ R/inputOutput.R
@@ -2,9 +2,20 @@
 #'
 #' @param symbol Name of symbol identifier
 #' @return named \code{internalIO} list item
-#' @seealso \code{moduleInput}
+#' @seealso \code{moduleIO}
 internalIO <- function(symbol) {
     internalIO <- list(symbol = symbol)
     class(internalIO) <- "internalIO"
     return(internalIO)
+}
+
+#' Create a file IO object
+#'
+#' @param ref Absolute or relative location of file
+#' @param path Optional search path for locating file
+#' @seealso \code{moduleIO}
+fileIO <- function(ref, path = NULL) {
+    fileIO <- list(ref = ref, path = path)
+    class(fileIO) <- "fileIO"
+    return(fileIO)
 }
