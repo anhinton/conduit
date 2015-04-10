741de9b6d85d7d39e30b592c4cead108923e3b6f
Ashley Noel Hinton <ahin017@aucklanduni.ac.nz>
2015-04-10 16:03:02 +1200

    created sriptVessel()

--- R/vessel.R
+++ R/vessel.R
@@ -40,7 +40,7 @@ NULL
 #' @export
 internalVessel <- function(symbol) {
     if (!is_length1_char(symbol)) {
-        stop(paste0("'symbol' is not a length 1 character vector"))
+        stop("'symbol' is not a length 1 character vector")
     }
     internalVessel <- list(symbol = symbol)
     class(internalVessel) <- c("internalVessel", "vessel")
@@ -80,7 +80,7 @@ internalVessel <- function(symbol) {
 #' @export
 fileVessel <- function(ref, path = NULL) {
     if (!is_length1_char(ref)) {
-        stop(paste0("'ref' is not a length 1 character vector"))
+        stop("'ref' is not a length 1 character vector")
     }
     if (!is.null(path)) {
         if (!is_length1_char(path)) {
@@ -90,4 +90,31 @@ fileVessel <- function(ref, path = NULL) {
     fileVessel <- list(ref = ref, path = path)
     class(fileVessel) <- c("fileVessel", "vessel")
     return(fileVessel)
+}
+
+#' Create a \code{scriptVessel} object.
+#'
+#' @details \code{scriptVessel} objects are used to store inline module
+#' source scripts.
+#'
+#' @param value Character vector of module script
+#'
+#' @return \code{scriptVessel}, \code{vessel} list object
+#'
+#' @seealso \code{vessel}, \code{moduleSource}, \code{source}
+#'
+#' @examples
+#' source2_script <-
+#'     scriptVessel(
+#'         value=c("print(\"Hello World!\")",
+#'                 "print(\"Goodbye cruel world...\")"))
+#'
+#' @export
+scriptVessel <- function(value) {
+    if (!is.character(value)) {
+        stop("'value' is not a character vector")
+    }
+    scriptVessel <- list(value = value)
+    class(scriptVessel) <- c("scriptVessel", "vessel")
+    return(scriptVessel)
 }
