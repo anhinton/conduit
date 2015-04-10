0df9c7536ea3bc5a217b3e2642c6aa0f9a2ca1a4
Ashley Noel Hinton <ahin017@aucklanduni.ac.nz>
2015-04-10 14:59:26 +1200

    created fileVessel()

--- R/vessel.R
+++ R/vessel.R
@@ -1,3 +1,14 @@
+#' Functions to manipulate vessels
+#'
+#' Functions to specify vessels for \code{moduleInput},
+#' \code{moduleOutput}, or \code{moduleSource} objects.
+#'
+#' @seealso \code{moduleInput}, \code{moduleOutput},
+#' \code{moduleSource}
+#' 
+#' @name vessel
+NULL
+
 #' Create an internal IO object
 #'
 #' @param symbol Name of symbol identifier
@@ -10,25 +21,41 @@ internalIO <- function(symbol) {
     return(internalIO)
 }
 
-#' Create a file IO object
+#' Create a \code{fileVessel} object.
+#'
+#' @details \code{fileVessel} objects are used to point to files on
+#' the local filesystem.
+#'
+#' These can be used to specify:
+#'
+#' \itemize{
+#'   \item{to where a \code{moduleOutput} is to be saved}
+#'   \item{from where a \code{moduleInput} is to retrieved}
+#'   \item{from where a \code{moduleSource} is to be retrieved}
+#' }
+#'
+#' \code{ref} (and \code{path}, if given), must be character vectors of
+#' length 1.
 #'
 #' @param ref Absolute or relative location of file
 #' @param path Optional search path for locating file
-#' @seealso \code{moduleIO}
-fileIO <- function(ref, path = NULL) {
+#'
+#' @return More about \code{vessel} objects, more on \code{fileVessel}
+#' \code{vessel} list objects, overview of \code{module} objects.
+#' @seealso \code{moduleInput}, \code{moduleOutput}, \code{moduleSource}
+#' 
+#' 
+#' @export
+fileVessel <- function(ref, path = NULL) {
     if (!is_length1_char(ref)) {
-        warning("'ref' is not a length 1 charachter vector")
-        warning("coercing 'ref' to length 1 character vector")
-        ref <- as_length1_char(ref)
+        stop(paste0("'ref' is not a length 1 character vector"))
     }
     if (!is.null(path)) {
         if (!is_length1_char(path)) {
-            warning("'path' is not a length 1 character vector")
-            warning("coercing 'path' to length 1 character vector")
-            path <- as_length1_char(path)
+            stop("'path' is not a length 1 character vector")
         }
     }
-    fileIO <- list(ref = ref, path = path)
-    class(fileIO) <- "fileIO"
-    return(fileIO)
+    fileVessel <- list(ref = ref, path = path)
+    class(fileVessel) <- c("fileVessel", "vessel")
+    return(fileVessel)
 }
