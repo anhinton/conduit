a4b7d40b8627fceef08e333bada3539b0af3b653
Ashley Noel Hinton <ahin017@aucklanduni.ac.nz>
2015-05-18 17:51:30 +1200

    create ensureModuleInput()

---
+++ R/script.R
@@ -0,0 +1,80 @@
+#' prepare internalVessel input script for python language
+internalVesselScript.python <- function (symbol, resource) {
+    
+}
+
+#' prepare internalVessel input script for R language
+internalVesselScript.R <- function (symbol, resource) {
+    script <- paste0(symbol, " <- readRDS(\"", resource, "\")")
+    return(script)
+}
+
+#' prepare internalVessel input script
+#'
+#' @param symbol character string with class set to language of module script
+#' @param resource file location of serialised language object
+#'
+#' @return character vector of script to ensure input
+internalVesselScript <- function (symbol, resource) {
+    UseMethod("internalVesselScript")
+}
+
+#' ensure input described by internalVessel is satisfied
+#'
+#' @param symbol
+#' @param resource
+#' @param language
+#'
+#' @return script as character vector
+#'
+#' @seealso \code{moduleInputScript}, \code{executeScript}
+ensureInternalVessel <- function (symbol, resource, language) {
+    class(symbol) <- language
+    script <- internalVesselScript(symbol, resource)
+    return(script)
+}
+
+#' ensure input described by fileVessel is satisfied
+#'
+#' @param ref file path to be satisfied
+#' @param resource file path to be passed to ref
+#'
+#' @return NULL
+ensureFileVessel <- function (ref, resource) {
+    ## check if ref already exists
+    if (file.exists(ref)) {
+        ## check if symlink
+        if (Sys.readlink(ref) == "") {
+            stop("input file already exists")
+        }
+        ## if ref is a symlink assume created by conduit and remove
+        file.remove(ref)
+    }
+
+    ##  create a symlink to resource at ref
+    file.symlink(resource, ref)
+    return(NULL)
+}
+
+#' convert module input to script
+#'
+#' @param input \code{moduleInput} object
+#' @param resource address of resource to be supplied as input
+#' @param language language of module script
+#'
+#' @return character string containing script to ensure input or NULL
+ensureModuleInput <- function (input, resource, language) {
+    vessel <- input$vessel
+    type <- switch(
+        class(vessel)[[1]],
+        internalVessel = "internal",
+        fileVessel = "file"
+    )
+    script <- switch(
+        type,
+        internal = ensureInternalVessel(vessel$symbol, resource, language),
+        file = ensureFileVessel(vessel$ref, resource)
+    )
+    
+    return(script)
+}
