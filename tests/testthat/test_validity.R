4382d987ce343c9d0cc56123d7344d69f51e426b
Ashley Noel Hinton <ahin017@aucklanduni.ac.nz>
2016-01-22 12:16:56 +1300

    revised, documented, tested validatePipe()

--- tests/testthat/test_validity.R
+++ tests/testthat/test_validity.R
@@ -8,9 +8,57 @@ layoutGraph.xml <- system.file("extdata", "simpleGraph", "layoutGraph.xml",
 pipeline.xml <- system.file("extdata", "simpleGraph",
                                           "pipeline.xml",
                                           package = "conduit")
+simpleGraph <- loadPipeline("simpleGraph", pipeline.xml)
+pipeList <- getPipes(simpleGraph)
+componentList <- getComponents(simpleGraph)
 invalidPipeline <- system.file("extdata", "invalidPipeline.xml",
                                package = "conduit")
 
+
+test_that("validatePipe() returns correctly", {
+    ## error for invalid input
+    expect_error(validatePipe(unclass(pipeList[[1]]), componentList),
+                 "pipe object required")
+    notAllComponents <- {
+        x <- componentList
+        x[[2]] <- unclass(x[[2]])
+        x
+    }
+    expect_error(validatePipe(pipeList[[1]], notAllComponents),
+                 "list of component objects required")
+
+    ## fails for missing startComponent
+    pipe1 <- pipeList[[1]]
+    pipe1$start$component <- tempfile()
+    expect_warning(valid1 <- validatePipe(pipe1, componentList),
+                   "Start component")
+    expect_false(valid1)
+
+    ## fails for missing startOutput
+    pipe2 <- pipeList[[1]]
+    pipe2$start$output <- tempfile()
+    expect_warning(valid2<- validatePipe(pipe2, componentList),
+                   "Start output")
+    expect_false(valid2)
+
+    ## fails for missing endComponent
+    pipe3 <- pipeList[[1]]
+    pipe3$end$component <- tempfile()
+    expect_warning(valid3 <- validatePipe(pipe3, componentList),
+                   "End component")
+    expect_false(valid3)
+
+    ## fails for missing endInput
+    pipe4 <- pipeList[[1]]
+    pipe4$end$input <- tempfile()
+    expect_warning(valid4 <- validatePipe(pipe4, componentList),
+                   "End input")
+    expect_false(valid4)
+
+    ## works when valid
+    expect_true(all(sapply(pipeList, validatePipe,
+                           componentList = componentList)))
+})
 
 test_that("isValidXML() works for known valid files", {
     expect_true(isValidXML(file = createGraph.xml, type = "module"))
