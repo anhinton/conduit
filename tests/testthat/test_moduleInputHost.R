60d7364a0f7c3d1762545c3c436fa57cbc679da3
Ashley Noel Hinton <ahin017@aucklanduni.ac.nz>
2016-05-13 12:48:48 +1200

    testing to run module with moduleInputHost

--- tests/testthat/test_moduleInputHost.R
+++ tests/testthat/test_moduleInputHost.R
@@ -1,6 +1,10 @@
 library(conduit)
 context("create and use moduleInputHost objects")
 
+## skip tests which require a vagrantHost be available with
+## Vagrantfile at ~/vagrant/vagrant-conduit/Vagrantfile
+skipVagrantHost <- TRUE
+
 name <- "myHost"
 
 test_that("moduleInputHost() constructor works right", {
@@ -43,100 +47,26 @@ test_that("moduleHostToXML.moduleInputHost() creates correct XML", {
     expect_match(attrs1[["name"]], name)
 })
 
-## test_that("prepareModuleHost.moduleInputHost() returns correctly", {
-##     if (skipHost) {
-##         skip(paste("tests requires a moduleInputHost running at",
-##                    "~/moduleInput/moduleInput-conduit/ModuleInputfile"))
-##     }
-    
-##     moduleInputHost1 <- moduleInputHost(moduleInputfile = moduleInputfile)
-##     moduleName1 <- "mod1"
-##     modulePath1 <- tempfile("modulePath")
-##     if (!dir.exists(modulePath1))
-##         dir.create(modulePath1)
-##     inputs1 <- sapply(1:3, function(x) tempfile(tmpdir = modulePath1))
-##     system2("touch", args = inputs1)
-##     outputLocation1 <-
-##         prepareModuleHost(moduleHost = moduleInputHost1,
-##                           moduleName = moduleName1,
-##                           modulePath = modulePath1)
-##     realLocation1 <- file.path(moduleInputHost1$hostdir, outputLocation1)
-##     expect_true(dir.exists(realLocation1))
-##     expect_is(outputLocation1, "moduleInputHostOutputLocation")
-##     expect_is(outputLocation1, "outputLocation")
-##     ## correct defined as contents of realLocation1 now same as
-##     ## modulePath1
-##     lapply(list.files(modulePath1),
-##            function (x) {
-##                expect_match(list.files(realLocation1), x, all = FALSE)
-##            })
-## })
+test_that("conduit can run a module with a moduleInputHost", {
+    if (skipVagrantHost) {
+        skip(paste("tests requires a vagrantHost running at",
+                   "~/vagrant/vagrant-conduit/Vagrantfile"))
+    }
+    mod1 <- loadModule("createHost",
+                       system.file("extdata", "moduleInputHostDemo",
+                                   "createHost.xml",
+                                   package = "conduit"))
+    mod2 <- loadModule("executeHost",
+                       system.file("extdata", "moduleInputHostDemo",
+                                   "executeHost.xml",
+                                   package = "conduit"))
+    res1 <- runModule(module = mod1, targetDirectory = tempdir())
+    inputObjects1 <- list(vagrantHost = res1$outputList$vagrantHost$ref)
 
-## test_that("executeCommand.moduleInputHost() returns correctly", {
-##     if (skipHost) {
-##         skip(paste("tests requires a moduleInputHost running at",
-##                    "~/moduleInput/moduleInput-conduit/ModuleInputfile"))
-##     }
-
-##     moduleInputHost1 <- moduleInputHost(moduleInputfile = moduleInputfile)
-##     mod1 <- module(name = "mod1",
-##                    language = "R",
-##                    host = moduleInputHost1,
-##                    sources = list(moduleSource(scriptVessel("x <- 1:10"))),
-##                    outputs = list(moduleOutput(
-##                        name = "x",
-##                        vessel = internalVessel("x"),
-##                        format = ioFormat("R numeric vector"))))
-##     modulePath1 <- tempfile("modulePath")
-##     if (!dir.exists(modulePath1))
-##         dir.create(modulePath1)
-##     oldwd <- setwd(modulePath1)
-##     on.exit(setwd(modulePath1))
-##     command1 <- command(prepareScript(mod1))
-##     outputLocation1 <- prepareModuleHost(moduleHost = moduleInputHost1,
-##                                         moduleName = getName(mod1),
-##                                         modulePath = modulePath1)
-##     exec_result <- executeCommand.moduleInputHost(moduleHost = moduleInputHost1,
-##                                               outputLocation = outputLocation1,
-##                                               command = command1)
-##     ## correct defined as not getting error message from wrapped
-##     ## system2 call
-##     expect_equal(exec_result, 0)
-## })
-
-## test_that("retrieveModuleHost.moduleInputHost() returns correctly", {
-##     if (skipHost) {
-##         skip(paste("tests requires a moduleInputHost running at",
-##                    "~/moduleInput/moduleInput-conduit/ModuleInputfile"))
-##     }
-##     moduleInputHost1 <- moduleInputHost(moduleInputfile = moduleInputfile)
-##     mod1 <- loadModule(name = "mod1",
-##                        ref = system.file(
-##                            "extdata", "test_pipeline",
-##                            "module1.xml",
-##                            package = "conduit"))
-##     mod1$host <- moduleInputHost1
-##     modulePath1 <- tempfile("modulePath")
-##     if (!dir.exists(modulePath1))
-##         dir.create(modulePath1)
-##     oldwd <- setwd(modulePath1)
-##     on.exit(setwd(modulePath1))
-##     command1 <- command(prepareScript(mod1))
-##     outputLocation1 <- prepareModuleHost(moduleHost = moduleInputHost1,
-##                                         moduleName = getName(mod1),
-##                                         modulePath = modulePath1)
-##     realLocation1 <- file.path(moduleInputHost1$hostdir, outputLocation1)
-##     exec_result <- executeCommand.moduleInputHost(moduleHost = moduleInputHost1,
-##                                               outputLocation = outputLocation1,
-##                                               command = command1)
-##     x <- retrieveModuleHost.moduleInputHost(moduleHost = moduleInputHost1,
-##                                         outputLocation = outputLocation1,
-##                                         modulePath = modulePath1)
-##     expect_null(x)
-##     ## correct defined as contents of modulePath1 and realLocation
-##     ## being the same, assuming they were not before execution
-##     lapply(list.files(realLocation1),
-##            function (x) {
-##                expect_match(list.files(modulePath1), x, all = FALSE)
-##            })
-## })
+    res2 <- runModule(module = mod2, targetDirectory = tempdir(),
+                      inputObjects = inputObjects1)
+    expect_is(res2, "moduleResult")
+    lapply(names(mod2$outputs), function(n) {
+        expect_match(names(res2$outputList), n, all = FALSE)
+    })
+})
