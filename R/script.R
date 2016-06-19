6a9ab554ef9389c3c430cc23b323abdaced2360a
Ashley Noel Hinton <ahin017@aucklanduni.ac.nz>
2016-06-20 11:05:27 +1200

    update command() doc

--- R/script.R
+++ R/script.R
@@ -296,7 +296,7 @@ executeScript <- function(script, moduleHost, outputLocation) {
 #'
 #' This function is usually called by \code{executeScript}.
 #'
-#' @param \code{script} object
+#' @param script \code{script} object
 #'
 #' @return \code{command} list containing \code{command} and
 #'     \code{args} character vectors
